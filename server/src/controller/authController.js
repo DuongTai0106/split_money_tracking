b3b86045ff33b3d6be6591836d579d1f981601c0
DuongTai0106 <duongthanhtai0106@gmail.com>
2026-01-04 20:55:27 +0700

    feat: delete token when logout

--- server/src/controller/authController.js
+++ server/src/controller/authController.js
@@ -181,8 +181,9 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "none",
     secure: process.env.NODE_ENV === "production",
+    path: "/",
   });
 
   res.json({ message: "Đăng xuất thành công" });
