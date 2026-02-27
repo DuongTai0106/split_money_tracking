29417100232d796b12937e17b9366fd0113cf557
DuongTai0106 <duongthanhtai0106@gmail.com>
2026-02-28 00:48:35 +0700

    feat: pwa for add to home screen

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate", // Tự động cập nhật khi bạn deploy code mới
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Bill Tracking",
+        short_name: "Bill Tracking",
+        description: "Ứng dụng chia bill cho nhóm",
+        theme_color: "#1a1715", // Màu thanh status bar trên điện thoại
+        background_color: "#1a1715", // Màu nền lúc khởi động app
+        display: "standalone", // Chế độ hiển thị full màn hình (không còn thanh URL)
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait", // Khóa chiều dọc (tùy chọn)
+        icons: [
+          {
+            src: "pwa-192x192.svg", // Đảm bảo bạn đã bỏ ảnh vào thư mục public
+            sizes: "192x192",
+            type: "image/svg",
+          },
+          {
+            src: "pwa-512x512.svg",
+            sizes: "512x512",
+            type: "image/svg",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     allowedHosts: true,
   },
